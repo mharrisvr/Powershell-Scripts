19277cb906ffa79e0262d04d88d2054d0dd3b585
Mike <mharrisvr@users.noreply.github.com>
2021-07-29 15:42:01 -0400

    Check if AD account exists
    Added try, catch, to $User variable and added if/else to check and write error if AD account does not exist

--- AD Scripts/PasswordChecker/UpdatedPasswordChecker.ps1
+++ AD Scripts/PasswordChecker/UpdatedPasswordChecker.ps1
@@ -9,20 +9,26 @@ Write-Verbose -Message "Checking to see if passwords have been changed.. `n" -Ve
 
 ForEach ( $UserObject in $UserObjects ) {
 
-    $User = Get-ADUSer $UserObject.Name -Properties *
+    $User = $(try {Get-ADUSer $UserObject.Name -Properties * } catch {$null})
     $ts = New-TimeSpan -Seconds 2 #Had to add to time to make comparison check work. 
     $CreationDate = $User.'Created' + $ts 
     $PwdSet = $User.'PasswordLastSet' 
     $Name = $User.'DisplayName'
     
-    if ($PwdSet -gt $CreationDate) {
-        Write-Host "$Name was created on: $CreationDate" 
-        Write-Host "Account Password last set on: $PwdSet" 
-        Write-Host "Account password has been changed. `n" -ForegroundColor Green      
-    }
-    else {
-        Write-Host "$Name was created on: $CreationDate" 
-        Write-Host "Account Password last set on: $PwdSet"
-        Write-Host ("Password has not been reset `n") -ForegroundColor Red 
+    if ($User -ne $Null) {
+    
+        if ($PwdSet -gt $CreationDate) {
+            Write-Host "$Name was created on: $CreationDate" 
+            Write-Host "Account Password last set on: $PwdSet" 
+            Write-Host "Account password has been changed. `n" -ForegroundColor Green      
+        }
+        else {
+            Write-Host "$Name was created on: $CreationDate" 
+            Write-Host "Account Password last set on: $PwdSet"
+            Write-Host ("Password has not been reset `n") -ForegroundColor Red 
         }
     }
+    else {
+        Write-Host "AD account does not exist yet for: " $UserObject.Name
+    }
+}
