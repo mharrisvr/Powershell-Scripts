dedac84b92a9801e365266b5cf87573c61186285
Mike <mharrisvr@users.noreply.github.com>
2023-01-23 16:13:37 -0500

    Update GroupCreator.ps1

--- GroupCreator.ps1
+++ GroupCreator.ps1
@@ -1,7 +1,7 @@
 # Creates new AD Group for Temp or perm admin rights & adds users into group. 
 # Will need to update both groupname, SamAccountName, and Members sections 
 
-$GroupNme = "Test Group"
+$GroupName = "Test Group"
 $GroupMembers = "user1, user2, user3"
 
 New-ADGroup -Name $GroupName -SamAccountName $GroupName -GroupCategory Security -GroupScope Global -Path "Insert Path Here"
