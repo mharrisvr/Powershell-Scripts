0441a694009ed35d1cc12e5c22a91c20fc1a73fb
Mike <mharrisvr@users.noreply.github.com>
2023-03-17 15:50:45 -0400

    Create ADAccountBulkCreator.ps1

---
+++ AD Scripts/ADAccountBulkCreator.ps1
@@ -0,0 +1,42 @@
+#https://blog.netwrix.com/2018/06/07/how-to-create-new-active-directory-users-with-powershell/
+#Saving here to modify later for personal project.. 
+
+#Enter a path to your import CSV file
+$ADUsers = Import-csv C:scriptsnewusers.csv
+
+foreach ($User in $ADUsers)
+{
+
+       $Username    = $User.username
+       $Password    = $User.password
+       $Firstname   = $User.firstname
+       $Lastname    = $User.lastname
+    $Department = $User.department
+       $OU           = $User.ou
+
+       #Check if the user account already exists in AD
+       if (Get-ADUser -F {SamAccountName -eq $Username})
+       {
+               #If user does exist, output a warning message
+               Write-Warning "A user account $Username has already exist in Active Directory."
+       }
+       else
+       {
+              #If a user does not exist then create a new user account
+          
+        #Account will be created in the OU listed in the $OU variable in the CSV file; don’t forget to change the domain name in the"-UserPrincipalName" variable
+              New-ADUser `
+            -SamAccountName $Username `
+            -UserPrincipalName "$Username@yourdomain.com" `
+            -Name "$Firstname $Lastname" `
+            -GivenName $Firstname `
+            -Surname $Lastname `
+            -Enabled $True `
+            -ChangePasswordAtLogon $True `
+            -DisplayName "$Lastname, $Firstname" `
+            -Department $Department `
+            -Path $OU `
+            -AccountPassword (convertto-securestring $Password -AsPlainText -Force)
+
+       }
+}
